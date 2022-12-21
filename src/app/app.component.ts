1c664d55cb3cbab9104d43d8b6b6963afbe68fec
Christiane Hertwig INVERS GmbH <christianehertwiginversgmbh@mbpchriehertwig.localdomain>
2022-12-21 11:39:43 +0100

    erster commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'untitled2';
+  title = "shoppinglist";
+
+  filter: "all" | "active" | "done" = "all";
+
+  Polnisch = [{ polnisch: "Dobranoc", done: true }];
+  Deutsch = [{ deutsch: "Gute Nacht", done: true }];
+
+  get woerterPolnisch() {
+    if (this.filter === "all") {
+      return this.Polnisch;
+    }
+    return this.Polnisch.filter((wortPolnisch) =>
+      this.filter === "done" ? wortPolnisch.done : !wortPolnisch.done
+    );
+  }
+
+  get woerterDeutsch() {
+    if (this.filter === "all") {
+      return this.Deutsch;
+    }
+    return this.Deutsch.filter((wortDeutsch) =>
+      this.filter === "done" ? wortDeutsch.done : !wortDeutsch.done
+    );
+  }
+
+  addPolnisch(polnisch: string) {
+    this.Polnisch.push({
+      polnisch,
+      done: false
+    });
+  }
+
+  addDeutsch(deutsch: string) {
+    this.Deutsch.push({
+      deutsch,
+      done: false
+    });
+  }
 }
